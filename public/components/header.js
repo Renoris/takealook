85c352982dcdd130915ce7a97f11d888f282bb66
Jacksieze <wsj23jack@naver.com>
2023-02-16 15:46:20 +0900

    [feature/net-37] 로그인 안내스크립트 및  내정보 박스 구현

--- public/components/header.js
+++ public/components/header.js
@@ -1,6 +1,7 @@
 // 로그인 화면 띄우기
 const loginBox = document.querySelector(".login_box");
 const sideBtn = document.querySelector("#menu_bar");
+const accountBox = document.querySelector(".account_box");
 
 sideBtn.addEventListener("click", function () {
   loginBox.classList.toggle("show");
@@ -11,28 +12,42 @@ const loginBtn = document.querySelector("#login_btn");
 
 loginBtn.addEventListener("click", function (e) {
   const login = document.getElementById("email").value;
+  const clear = document.querySelector(".clear");
+  const fail = document.querySelector(".fail");
 
   if (login == "") {
     alert("이메일 입력하삼");
     e.preventDefault();
     return false;
   } else if (login.length <= 11) {
-    alert("이메일 쓴거 맞음?");
+    fail.classList.add("confirm_log_show");
+    clear.classList.remove("confirm_log_show");
     e.preventDefault();
     return false;
-  } else {
-    alert("어서오시고~:)");
-    return true;
+  } else if (login.length >= 12) {
+    clear.classList.add("confirm_log_show");
+    fail.classList.remove("confirm_log_show");
+    e.preventDefault();
   }
 });
 
 // 검색바 나오게 하기
-const searchBar = document.querySelector("#search_bar");
-const searchBtn = document.querySelector("#search_btn");
+const searchReturn = document.querySelector(".back");
+const searchBar = document.querySelector(".search_bar");
+const searchBtn = document.querySelector(".search_btn");
 
-searchBtn.addEventListener("mouseover", function () {
-  searchBar.style.display = "block";
-  searchBar.addEventListener("mouseout", function () {
-    searchBar.style.display = "none";
-  });
+searchBtn.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault();
+    searchBar.classList.add("search_bar_show");
+    searchBtn.classList.add("search_btn_show");
+    searchReturn.style.display = "block";
+  },
+  { once: true }
+);
+searchReturn.addEventListener("click", function () {
+  searchBar.classList.remove("search_bar_show");
+  searchBtn.classList.remove("search_btn_show");
+  searchReturn.style.display = "none";
 });
