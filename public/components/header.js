55bdab0283a76eb595cdb9020ff8f1f1888e9545
applet <celbever@naver.com>
2023-02-17 17:35:32 +0900

    [feature/net-41] 로그인 버튼 기능 구현

--- public/components/header.js
+++ public/components/header.js
@@ -3,35 +3,75 @@ const loginBox = document.querySelector(".login_box");
 const sideBtn = document.querySelector("#menu_bar");
 const accountBox = document.querySelector(".account_box");
 
-sideBtn.addEventListener("click", function () {
-  loginBox.classList.toggle("show");
-});
+
+
+
+/**
+ * 로그인 이메일 값 검사
+ * @param email : string 이메일
+ * @param clear : Element clear class Element
+ * @param fail : Element fail class Element
+ * @param event event
+ */
+function validateEmail(email, clear, fail, event) {
+  // if (email === "") {
+  //   alert("이메일을 입력하세요.");
+  //   clear.classList.remove("confirm_log_show");
+  //   fail.classList.remove("confirm_log_show");
+  //   return false;
+  // }
+  // if (email.length <= 11) {
+  //   fail.classList.add("confirm_log_show");
+  //   clear.classList.remove("confirm_log_show");
+  //   return false;
+  // }
+  // if (email.length >= 12) {
+  //   clear.classList.add("confirm_log_show");
+  //   fail.classList.remove("confirm_log_show");
+  //   return false;
+  // }
+  return true;
+}
+
+async function requestLoginEvent(e) {
+  const email = document.getElementById("email").value;
+  const clear = document.querySelector(".clear");
+  const fail = document.querySelector(".fail");
+
+  if (validateEmail(email, clear, fail, e)) {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/auth', {method : 'POST',
+          headers : {'Content-Type' : 'application/json'} ,body : JSON.stringify({email: email})});
+      if (!(response.status === 200)) throw Error();
+      const {success} = await response.json();
+      if (!success) throw Error();
+      fail.classList.add("confirm_log_show");
+      clear.classList.remove("confirm_log_show");
+    } catch (error) {
+      fail.classList.remove("confirm_log_show");
+      clear.classList.add("confirm_log_show");
+    }
+  }
+}
+
+async function sideBtnOnclickEvent() {
+  const access = localStorage.getItem('takealook-access');
+  const refresh = localStorage.getItem('takealook-refresh');
+  if (!access || !refresh) {  loginBox.classList.toggle("show"); return}
+  fetch('/api/member')
+}
+
+
+sideBtn.addEventListener("click", sideBtnOnclickEvent);
+
 
 // 로그인 입력 반응
 const loginBtn = document.querySelector("#login_btn");
 //
-loginBtn.addEventListener("click", function (e) {
-  const login = document.getElementById("email").value;
-  const clear = document.querySelector(".clear");
-  const fail = document.querySelector(".fail");
 
-  if (login == "") {
-    alert("이메일 입력하삼");
-    e.preventDefault();
-    clear.classList.remove("confirm_log_show");
-    fail.classList.remove("confirm_log_show");
-    return false;
-  } else if (login.length <= 11) {
-    fail.classList.add("confirm_log_show");
-    clear.classList.remove("confirm_log_show");
-    e.preventDefault();
-    return false;
-  } else if (login.length >= 12) {
-    clear.classList.add("confirm_log_show");
-    fail.classList.remove("confirm_log_show");
-    e.preventDefault();
-  }
-});
+
+loginBtn.addEventListener("click", requestLoginEvent);
 
 // 검색바 나오게 하기
 const searchReturn = document.querySelector(".back");
