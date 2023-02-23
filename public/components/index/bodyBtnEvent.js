502fdbf08e25067ebfbfc138372b592dbac1a62f
applet <celbever@naver.com>
2023-02-23 20:25:32 +0900

    [feature/net-62] 이미지 드래그 제거

--- public/components/index/bodyBtnEvent.js
+++ public/components/index/bodyBtnEvent.js
@@ -1,13 +1,33 @@
 import elementFactory from "../elements/MoviesElements.js";
 import authfetch from "../fetchs/authfetch.js";
 
+function getButtonState(state, node) {
+    let _state = state;
+    return {
+        getState() {
+            return _state;
+        },
+
+        turnState : function () {
+            if (_state) {
+                node.src = "./images/fav_on.png";
+            } else {
+                node.src = "./images/fav_off.png";
+            }
+            _state = !_state;
+        }
+    }
+}
+
+
+
+
 export async function pickMovie (movieId, access) {
     if (!access) {
         alert("로그인이 되어잇지 않습니다.");
         return;
     }
 }
-
 
 export async function spreadMovieInfo(params, access) {
     try {
@@ -26,11 +46,15 @@ export async function spreadMovieInfo(params, access) {
 
         const $movieContainer = document.querySelector('.movie_container');
         result.forEach((movie) => {
+
             const movieCover = elementFactory.createMovieCoverNode($movieContainer);
-            elementFactory.createPickButtonNode(movie.id, movieCover);
+            const pickButton = elementFactory.createPickButtonNode(movie.id, movieCover);
             const coverImageNode = elementFactory.createCoverImageNode(movieCover);
             elementFactory.createInCoverImageNode(movie.image, coverImageNode);
             elementFactory.createMovieInfoNode(movie.title, movie.genre, movieCover);
+
+            const buttonState = getButtonState(false, pickButton);
+            pickButton.addEventListener('click', (e) => buttonState.turnState());
         });
 
         console.log(result);
