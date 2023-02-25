ad8a581aa81e689f9e978672210d5525c2a70dca
applet <celbever@naver.com>
2023-02-25 17:59:41 +0900

    [feature/net-63] 픽 라우터 등록

--- routes/api/ApiRouter.js
+++ routes/api/ApiRouter.js
@@ -6,18 +6,17 @@ const {accessFilter} = require('../filter/JwtFilter');
 const memberRouter = require('./member/MemberRouter');
 const testRouter = require('./test/TestRouter');
 const signUpRouter = require('./member/SignUpRouter');
-
+const pickRouter = require('/pick/PickRouter');
 
 //jwt 필요없음
 router.use('/auth', authRouter);
 router.use('/movie', movieRouter);
 router.use('/signup', signUpRouter);
 router.use('/test', testRouter);
-
 //이 아래로 jwt 필요
 //req.body.authId 에 유저 id 첨가
 router.use(accessFilter);
 
 router.use('/member', memberRouter);
-
+router.use('/pick', pickRouter);
 module.exports = router;
