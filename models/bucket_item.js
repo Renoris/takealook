402994012172e69d07695381717381b873f3b0d1
applet <celbever@naver.com>
2023-02-27 20:07:51 +0900

    [feature/net-68] 테이블 association 설정

--- models/bucket_item.js
+++ models/bucket_item.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      bucket_item.belongsTo(models.bucket);
+      bucket_item.belongsTo(models.movie);
     }
   }
   bucket_item.init({
