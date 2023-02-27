651b69644134a079836176f8bd0f33afda816218
applet <celbever@naver.com>
2023-02-27 15:30:25 +0900

    [hotfix/개인db작업] 개인 db 접속으로 전환

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: 'zkfp3445@',
-    database: 'takealook',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     timezone: "+09:00", // 타임존 설정안하면 1981년 1월 1일로 하면 1980년 12월 31일 15:00 가 된다.
   },
