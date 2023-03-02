535aa47706e2c49a518539885e478885a12432d3
Jacksieze <wsj23jack@naver.com>
2023-03-02 17:51:54 +0900

    [feature/net-75] 이메일 로그인 전송 수정완료 / 헤더 로그인 및 반응형 스크립트 적용

--- public/components/index/header.js
+++ public/components/index/header.js
@@ -1,5 +1,6 @@
 import {
   sideBtnClickEventListener,
+  outerBtnClickEventListener,
   logoutBtnClickEventListener,
   loginBtnClickEventListener,
   searchBtnClickEventListener,
@@ -10,8 +11,11 @@ import {
 // 로그인 화면 띄우기
 const loginBox = document.querySelector(".login_box");
 const outerLogin = document.querySelector(".outer_login");
+const outerProfile = document.querySelector(".outer_profile");
 const sideBtn = document.querySelector("#menu_bar");
 const accountBox = document.querySelector(".account_box");
+const outerAccount = document.querySelector(".outer_account");
+const outerLogout = document.querySelector(".outer_logout");
 const logoutBtn = document.getElementById("logout_btn");
 const loginBtn = document.querySelector("#login_btn");
 const email = document.getElementById("email");
@@ -26,8 +30,30 @@ const vaildEmail = document.querySelector(".valid_email");
 const top = document.querySelector(".up_to_top");
 
 logoutBtn.addEventListener("click", (e) => logoutBtnClickEventListener(e));
+outerLogout.addEventListener("click", (e) => logoutBtnClickEventListener(e));
 sideBtn.addEventListener("click", (e) => sideBtnClickEventListener(e, accountBox, loginBox));
-outerLogin.addEventListener("click", (e) => sideBtnClickEventListener(e, accountBox, loginBox));
+outerLogin.addEventListener("click", (e) =>
+  outerBtnClickEventListener(
+    e,
+    accountBox,
+    loginBox,
+    outerLogin,
+    outerProfile,
+    outerAccount,
+    outerLogout
+  )
+);
+outerProfile.addEventListener("click", (e) =>
+  outerBtnClickEventListener(
+    e,
+    accountBox,
+    loginBox,
+    outerLogin,
+    outerProfile,
+    outerAccount,
+    outerLogout
+  )
+);
 // 로그인 입력 반응
 loginBtn.addEventListener("click", (e) =>
   loginBtnClickEventListener(e, email, clear, fail, noAccount, vaildEmail)
@@ -39,3 +65,22 @@ searchReturn.addEventListener("click", (e) =>
   searchReturnClickEventListener(e, searchBar, searchBtn, searchView, searchReturn)
 );
 window.addEventListener("scroll", (e) => topScrollEventListener(e, top));
+
+// 로그인 전, 후 UI 변경
+function onloadpage() {
+  const access = localStorage.getItem("takealook-access");
+  const refresh = localStorage.getItem("takealook-refresh");
+
+  if (!(access && refresh)) {
+    outerLogin.classList.add("outer_show");
+    outerAccount.classList.add("outer_show");
+    outerProfile.classList.remove("outer_hide");
+    outerLogout.classList.remove("outer_hide");
+  } else {
+    outerProfile.classList.add("outer_show");
+    outerLogout.classList.add("outer_show");
+    outerLogin.classList.remove("outer_hide");
+    outerAccount.classList.remove("outer_hide");
+  }
+}
+onloadpage();
