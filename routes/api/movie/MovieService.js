533f896a57225ec0a235230cb1158097f2570ddc
applet <celbever@naver.com>
2023-03-08 01:45:26 +0900

    [feature/net-102] 버그수정

--- routes/api/movie/MovieService.js
+++ routes/api/movie/MovieService.js
@@ -1,8 +1,12 @@
 const movieStorage = require('./MovieStorage');
-
+const crawling = require('../../../util/crawling/crawling');
 
 const MovieService = {
     getMovies : async function (memberId, genre, pubDate, search ,limit, offset) {
+        if (search) {
+            await crawling.naverMovieApiCrawling(search);
+        }
+
         return movieStorage.getMovies(memberId, genre, pubDate, search ,limit, offset);
     },
 
